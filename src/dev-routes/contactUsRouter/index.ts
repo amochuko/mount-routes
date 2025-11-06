417aaea01b72a927b18ffce9a5a320b4c7a6cbce
amochuko <7204868+amochuko@users.noreply.github.com>
2025-11-06 08:41:42 +0100

    update route content

--- src/dev-routes/contactUsRouter/index.ts
+++ src/dev-routes/contactUsRouter/index.ts
@@ -3,7 +3,7 @@ import express from "express";
 const router = express.Router({ mergeParams: true });
 
 router.get("/", (req, res) => {
-  res.json({ data: "Contact us on our telephone lines: +234 9097 474 8383" });
+  res.json({ data: "Contact us on our telephone lines: +001 9097 474 8383" });
 });
 
 export default router;
